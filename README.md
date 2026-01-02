593d51d08cb499addc2579b93ebc35ecdabeeff3
Rémy Potus <remy.potus@gmail.com>
2026-01-02 14:59:43 +0100

    ✨ find the answer !!

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This repo wil contain all the data for each of the Advent of code's exercise that I have time do resolve during my free time.
+
+First day : 
+- First answer : 1097
--- day-1/secret-entrance.ts
+++ day-1/secret-entrance.ts
@@ -3,7 +3,7 @@ import { DialManager } from './dial-manager.class';
 const realInputFile = 'puzzle-input.txt';
 const exampleInputFile = 'example-input.txt'
 
-const dialManager = new DialManager(exampleInputFile);
+const dialManager = new DialManager(realInputFile);
 
 const password = dialManager.findPassword();
 
