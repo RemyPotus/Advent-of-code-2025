d03597b3d0c523c961a49d11d79195567ef83078
Rémy Potus <remy.potus@gmail.com>
2026-01-02 13:27:37 +0100

    ✨ map input to have clean data

--- day-1/secret-entrance.ts
+++ day-1/secret-entrance.ts
@@ -1,7 +1,16 @@
 import * as fs from 'fs';
+import { Rotation } from './rotation.model';
 
 const INPUT: string = fs.readFileSync('./day-1/puzzle-input.txt','utf-8');
 
-const rotations = INPUT.split("/n")
+const rotations: string[] = INPUT.split(/\r?\n/);
 
-console.log('Rotations = ',rotations)
+const cleanRotations: Rotation[] = rotations.map(rotation => {
+  const cleanInput: string= rotation.trim();
+  return {
+    direction: cleanInput.substring(0,1),
+    value: parseInt(cleanInput.substring(1, cleanInput.length)),
+  } as Rotation
+});
+
+console.log(cleanRotations[1],cleanRotations[5])
