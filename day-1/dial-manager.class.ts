54679cfc55221be3089da961f4d623bb5fb1e778
Rémy Potus <remy.potus@gmail.com>
2026-01-02 14:04:59 +0100

    ♻️ create class & methods

---
+++ day-1/dial-manager.class.ts
@@ -0,0 +1,64 @@
+import { Direction } from "./direction.enum";
+import { Rotation } from "./rotation.model";
+import * as fs from 'fs';
+
+export class DialManager {
+  public inputFile: string;
+  private zeroCount = 0;
+  private dialPosition = 0; 
+
+  public constructor(inputfile: string){
+    this.inputFile = inputfile;
+  }
+
+
+  public findPassword(): number{
+    return this.zeroCount;
+  };
+
+
+
+  private cleanInput(): string[] {
+    const INPUT: string = fs.readFileSync(`'./day-1/'${this.inputFile}`,'utf-8');
+    const rotations: string[] = INPUT.split(/\r?\n/);
+    return rotations;
+ 
+  }
+
+  private mapToRotations(stringRotations: string[]):Rotation[] {
+    const cleanRotations: Rotation[] = stringRotations.map(rotation => {
+      const cleanInput: string= rotation.trim();
+      return {
+        direction: cleanInput.substring(0,1) as Direction,
+        value: parseInt(cleanInput.substring(1, cleanInput.length)),
+      }
+    });
+    return cleanRotations;
+  };
+
+  private computeNextRotation(rotation: Rotation): void{
+    if(rotation.direction === Direction.Left){
+      this.computeLeftRotation(rotation.value);
+    }else {
+      this.computeRightRotation(rotation.value);
+    }
+
+    this.zeroCountIncrement();
+  }
+
+  private computeRightRotation(value: number){
+    const newPosition = this.dialPosition + value;
+    this.dialPosition = newPosition > 99 ? newPosition + 100 : newPosition
+  };
+
+  private computeLeftRotation(value: number) {
+    const newPosition = this.dialPosition + value;
+    this.dialPosition = newPosition < 0 ? newPosition + 100 : newPosition
+  };
+
+  private zeroCountIncrement(): void{
+    if(this.dialPosition === 0){
+      this.zeroCount++;
+    } 
+  }
+}
--- day-1/secret-entrance.ts
+++ day-1/secret-entrance.ts
@@ -1,16 +1,5 @@
-import * as fs from 'fs';
+import { DialManager } from './dial-manager.class';
 import { Rotation } from './rotation.model';
 
-const INPUT: string = fs.readFileSync('./day-1/puzzle-input.txt','utf-8');
+const dialManager = new DialManager('puzzle-InputDeviceInfo.txt');
 
-const rotations: string[] = INPUT.split(/\r?\n/);
-
-const cleanRotations: Rotation[] = rotations.map(rotation => {
-  const cleanInput: string= rotation.trim();
-  return {
-    direction: cleanInput.substring(0,1),
-    value: parseInt(cleanInput.substring(1, cleanInput.length)),
-  } as Rotation
-});
-
-console.log(cleanRotations[1],cleanRotations[5])
